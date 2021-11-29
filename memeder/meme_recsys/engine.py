d57ca5443c4466e7f0c6ac265fea111eed9d6464
ivan <anivan9889@gmail.com>
2021-11-29 14:32:46 +0300

    FEAT: add function to stole meme (engine.py)

--- memeder/meme_recsys/engine.py
+++ memeder/meme_recsys/engine.py
@@ -5,6 +5,21 @@ import numpy as np
 
 from memeder.paths import get_lib_root_path
 
+from telethon import TelegramClient, events, sync
+# stole memes from Fast Food Memes bot
+
+api_id = 15806656
+api_hash = 'f5fc3ffced16b257c1b36aace17014b9'
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+
+def meme_generator():
+    messages = client.get_messages('ffmemesbot')
+    msg = messages[0]
+    meme_id = msg.photo.id
+    messages[0].click()
+    return meme_id
 
 def get_list_of_memes():
     files = os.listdir(get_lib_root_path() / 'Memes')
@@ -12,12 +27,12 @@ def get_list_of_memes():
     return tuple(files)
 
 
-def meme_generator(list_memes=get_list_of_memes()):
-    i = 0
-    l = len(list_memes)
-    while True:
-        yield list_memes[i % l]
-        i += 1
+# def meme_generator(list_memes=get_list_of_memes()):
+#     i = 0
+#     l = len(list_memes)
+#     while True:
+#         yield list_memes[i % l]
+#         i += 1
 
 
 def get_random_meme():
--- stole_memes/client.py
+++ stole_memes/client.py
@@ -6,7 +6,9 @@ api_hash = 'f5fc3ffced16b257c1b36aace17014b9'
 client = TelegramClient('session_name', api_id, api_hash)
 client.start()
 
-while True:
+def meme_stole():
     messages = client.get_messages('ffmemesbot')
     msg = messages[0]
+    meme_id = msg.photo.id
     messages[0].click()
+    return meme_id
