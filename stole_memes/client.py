a150bbc95b81a8143639d820c0dea65bc1d86ba9
ivan <anivan9889@gmail.com>
2021-12-05 19:31:56 +0300

    update deployment files (without pizding memes)

--- stole_memes/client.py
+++ stole_memes/client.py
@@ -1,14 +1,14 @@
-from telethon import TelegramClient, events, sync
-
-api_id = 15806656
-api_hash = 'f5fc3ffced16b257c1b36aace17014b9'
-
-client = TelegramClient('session_name', api_id, api_hash)
-client.start()
-
-def meme_stole():
-    messages = client.get_messages('ffmemesbot')
-    msg = messages[0]
-    meme_id = msg.photo.id
-    messages[0].click()
-    return meme_id
+# from telethon import TelegramClient, events, sync
+#
+# api_id = 15806656
+# api_hash = 'f5fc3ffced16b257c1b36aace17014b9'
+#
+# client = TelegramClient('session_name', api_id, api_hash)
+# client.start()
+#
+# def meme_stole():
+#     messages = client.get_messages('ffmemesbot')
+#     msg = messages[0]
+#     meme_id = msg.photo.id
+#     messages[0].click()
+#     return meme_id
