3f1235f7c45765f25566ff873f59cc2d5563c889
BorisShirokikh <bg.dustpelt@gmail.com>
2022-01-21 11:42:21 +0300

    FEAT: test env credentials

--- memeder/database/config.py
+++ memeder/database/config.py
@@ -1,9 +1,7 @@
-host = 'ec2-3-89-214-80.compute-1.amazonaws.com'
-database = 'd988ab3t3bor26'
-user = 'vgxntsnysemgva'
-password = '94cee2e6d74132ab2eeafb4cf8af683ec9802cf47d935dae1eee421e17f8a356'
+import os
 
-host_test = 'ec2-23-21-229-200.compute-1.amazonaws.com'
-database_test = 'd80m409qj3pu8s'
-user_test = 'jffyzilbdtfxua'
-password_test = 'f7789ebaf7eea860725062241d3677b0030a61e772e6421fa71e1876b82fa22b'
+
+host = os.environ.get('DATABASE_HOST')
+database = os.environ.get('DATABASE_DATABASE')
+user = os.environ.get('DATABASE_USER')
+password = os.environ.get('DATABASE_PASSWORD')
--- memeder/main.py
+++ memeder/main.py
@@ -1,30 +1,26 @@
 import argparse
+import os
 
 import telebot
 
 from memeder.content_scheduler import start, process, receive_meme
-from memeder.bot_credentials import token, token_test
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--test', required=False, action='store_true', default=False)
-    args = parser.parse_known_args()[0]
-    is_test = args.test
+    token = os.environ.get('MEMEDATINGTESTBOT_TOKEN')
 
-    _token = token_test if is_test else token
-    bot = telebot.TeleBot(_token)
+    bot = telebot.TeleBot(token)
 
     # TODO: how can we check (and refresh) the current state?
     #  ... a "bug" in @ffmemesbot with double meme caused by double /start command without reacting on prev. meme
 
     @bot.message_handler(commands=['start'])
     def _start(message):
-        start(message, bot, force_start=True, test=is_test)
+        start(message, bot, force_start=True)
 
     @bot.callback_query_handler(func=lambda call: True)
     def _handle_meme_reply(call):
-        process(call, bot, test=is_test)
+        process(call, bot)
         bot.answer_callback_query(call.id)
 
     @bot.message_handler(content_types=['photo'])
