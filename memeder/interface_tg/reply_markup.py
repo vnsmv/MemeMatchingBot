10118ef4d2edceb1de7a337e221433486dd05177
ivan <anivan9889@gmail.com>
2022-01-31 23:36:53 +0300

    test new interface

--- memeder/interface_tg/reply_markup.py
+++ memeder/interface_tg/reply_markup.py
@@ -16,7 +16,7 @@ def get_reply_markup(stage=0):
     if (stage == 0) or (stage == 5):
         if stage == 0:
             message_body = 'Configure your profile to find your best match! \n' \
-                           '· Sex \U0001F466  \U0001F467 \n' \ 
+                           '· Sex \U0001F466  \U0001F467 \n' \
                            '· Preferences for dating \U0001F498  \n' \
                            '· Describe yourself! \U0001F58A \n' \
                            '· Share your photo \U0001F320'
