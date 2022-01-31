0e1be1a23ab42f088f834c2d9c4793956ece3cbf
ivan <anivan9889@gmail.com>
2022-01-31 22:08:50 +0300

    test new interface

--- memeder/interface_tg/config.py
+++ memeder/interface_tg/config.py
@@ -1,8 +1,11 @@
 
 
 MEME_REACTION2BUTTON = {
-    'b2_like':       ('\U0001F4A3' + '\U0001F4A5' + '\U0001F4AF',                               '1000'),
-    'b2_dislike':    (3 * '\U0001F4A9',                                                         '1001'),
+    # 'b2_like':       ('\U0001F4A3' + '\U0001F4A5' + '\U0001F4AF',                               '1000'),
+    # 'b2_dislike':    (3 * '\U0001F4A9',                                                         '1001'),
+
+    'b2_like':       ('\U0001F602',                                                             '1000'),
+    'b2_dislike':    ('\U0001F615',                                                         '1001'),
 
     'b1_rofl':       ('\U0001F923',                                                              '100'),
     'b1_smiling':    ('\U0001F600',                                                              '101'),
@@ -10,7 +13,8 @@ MEME_REACTION2BUTTON = {
     'b1_blushing':   ('\U0000263A',                                                              '103'),
     'b1_upside':     ('\U0001F643',                                                              '104'),
     'b1_thinking':   ('\U0001F914',                                                              '105'),
-    'b1_neutral':    ('\U0001F610',                                                              '106'),
+    # 'b1_neutral':    ('\U0001F610',                                                              '106'),
+    'b1_neutral':    ('\U0001F610	',                                                              '106'),
     'b1_yawning':    ('\U0001F971',                                                              '107'),
 
     'bu_users':      ('Give me the best match',                                                  'user'),
