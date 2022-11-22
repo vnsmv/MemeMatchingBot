6eb77b29eb814a6c3715a285b80401e8d426fa19
justfollowthesun <74874227+justfollowthesun@users.noreply.github.com>
2022-11-22 23:26:09 +0300

    Update connect.py

--- memeder/database/connect.py
+++ memeder/database/connect.py
@@ -17,15 +17,10 @@ def connect_to_db(env_file=None):
                          f'(`None`, `db_credentials.env`, `db_credentials_test.env`). '
                          f'However, {env_file} is given.')
 
-#     host = os.environ.get('DATABASE_HOST')
-#     database = os.environ.get('DATABASE_DATABASE')
-#     user = os.environ.get('DATABASE_USER')
-#     password = os.environ.get('DATABASE_PASSWORD')
-
-    host = 'ec2-3-217-251-77.compute-1.amazonaws.com'
-    database = 'd1htorqm1j6lui'
-    user = 'fbxnpgqlphzhwu'
-    password = 'eae0e8bb3444c423bb944a5829fb3d8791a9ad77a71bf4c5c77556154a40549c'
+    host = os.environ.get('DATABASE_HOST')
+    database = os.environ.get('DATABASE_DATABASE')
+    user = os.environ.get('DATABASE_USER')
+    password = os.environ.get('DATABASE_PASSWORD')
 
     connection = psycopg2.connect(host=host, database=database, user=user, password=password)
     cursor = connection.cursor()
